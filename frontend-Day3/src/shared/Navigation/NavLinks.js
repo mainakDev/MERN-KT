953637997c974ae8c4ab2021ca3fa7cd70ed7284
mainakDev <75646553+mainakDev@users.noreply.github.com>
2024-07-31 17:59:27 +0530

    Day-3 Mobile navigation

--- frontend-Day3/src/shared/Navigation/NavLinks.js
+++ frontend-Day3/src/shared/Navigation/NavLinks.js
@@ -6,7 +6,10 @@ import './NavLinks.css'
 const NavLinks = () => {
     return(
         <ul className='nav-links'>
-            <li></li>
+            <li><NavLink to='/' exact>ALL USERS</NavLink></li>
+            <li><NavLink to='/u1/places' >MY PLACES</NavLink></li>
+            <li><NavLink to='/places/new' >ADD PLACE</NavLink></li>
+            <li><NavLink to='/auth' >AUTHENTICATE</NavLink></li>
         </ul>
     );
 }
--- frontend-Day3/src/shared/Navigation/SlideDrawer.js
+++ frontend-Day3/src/shared/Navigation/SlideDrawer.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 
 import './SlideDrawer.css'
 
-const SlideDrawer = () => {
+const SlideDrawer = props => {
+    const content = <aside className='side-drawer'>{props.children}</aside>
 
+    return ReactDOM.createPortal(content, document.getElementById('drawer-hook'))
 }
 
 export default SlideDrawer;
